37ef7c345ff0dd1e224734e55aad1289939ed8fb
jaquelinepires <piresjaqueline17@gmail.com>
2024-03-05 16:19:41 -0300

    feat: added to ingredient list

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,59 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Alert } from 'react-native';
+import { styles } from './styles';
+import { Ingredients } from '@/components/Ingredients';
+import { useState } from 'react';
+import { Ingredient } from '@/components/Ingredient';
+import { Selected } from '@/components/selected';
 
 export default function Index() {
+    const [selected, setSelected] = useState<string[]>([])
+
+    function handleToggleSelected(value: string) {
+        if (selected.includes(value)) {
+          return setSelected((state) => state.filter((item) => item !== value))
+        }
+    
+        setSelected((state) => [...state, value])
+      }
+
+      function handleClearSelected() {
+        Alert.alert("Limpar", "Deseja limpar tudo?", [
+          { text: "Não", style: "cancel" },
+          { text: "Sim", onPress: () => setSelected([]) },
+        ])
+      }
+
     return (
-        <View>
-            <Text>Index</Text>
+        <View style={styles.container}>
+        <Text style={styles.title}>
+          Escolha {"\n"}
+          <Text style={styles.subtitle}>os produtos</Text>
+        </Text>
+  
+        <Text style={styles.message}>
+          Descubra receitas baseadas nos produtos que você escolheu.
+        </Text>
+    <ScrollView 
+    contentContainerStyle={styles.ingredient}
+    showsVerticalScrollIndicator={false}>
+
+        {Array.from({ length: 10 }).map((_item, index) => (
+        <Ingredient 
+            key={index} 
+            name="Tomate" 
+            image="" 
+            selected={selected.includes(String(index))} 
+         onPress={() => handleToggleSelected(String(index))}
+         />
+      ))}
+            </ScrollView>
+
+            {selected.length > 0 && (
+            <Selected 
+                quantity={selected.length} 
+                onClear={handleClearSelected} 
+                onSearch={() => {}} />
+            )}
         </View>
     );
 }
